c1937d81e1ee843dc7344057881f66ad60dcd1b0
tairasoul <100938045+tairasoul@users.noreply.github.com>
2024-11-24 12:00:53 +0200

    tell the user what port it's starting on

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@ using VapSRServer;
 
 int port = int.Parse(System.Environment.GetEnvironmentVariable("VAPSR_SERVER_PORT", EnvironmentVariableTarget.Process) ?? "7777");
 
+Console.WriteLine($"Starting server on port {port}");
+
 ServerHandler server = new(port);
 
 await server.Start();
